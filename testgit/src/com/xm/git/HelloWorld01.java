6bcf987135dbfba24bdc36fd23ad54cb376aebc5
xmm <875591584@qq.com>
2021-05-17 09:07:22 +0800

    修改01

--- testgit/src/com/xm/git/HelloWorld01.java
+++ testgit/src/com/xm/git/HelloWorld01.java
@@ -3,6 +3,6 @@ package com.xm.git;
 public class HelloWorld01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("修改");
+        System.out.println("修改1");
     }
 }
